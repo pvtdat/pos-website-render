b43f18de46a6c391b2da8c97af447ed107dd8cf2
datit <phamvantiendat.work@gmail.com>
2024-07-03 00:59:45 +0700

    config change

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,8 +3,9 @@ module.exports = function (app) {
   app.use(
     ["/api"],
     createProxyMiddleware({
-      target: "http://localhost:8080",
-      changeOrigin: true,
+      // target: "http://localhost:8080",
+        target: "https://pos-website-render.onrender.com",
+        changeOrigin: true,
     })
   );
 };
