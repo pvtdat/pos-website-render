20a07e8c42233bcd7dc1dbf49ea23d7ddecc3402
datit <phamvantiendat.work@gmail.com>
2024-07-08 22:35:10 +0700

    Update CorsFilter.java

--- src/main/java/com/finalpos/POSsystem/Config/CorsFilter.java
+++ src/main/java/com/finalpos/POSsystem/Config/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
